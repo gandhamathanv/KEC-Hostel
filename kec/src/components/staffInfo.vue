11d3ab7219b90da8868ff43c5be301f0fa461242
gandhamathan <gandhamathanv.20cse@kongu.edu>
2022-04-23 20:57:49 +0530

    add commit

---
+++ kec/src/components/staffInfo.vue
@@ -0,0 +1,333 @@
+<template>
+  <div>
+    <section>
+      <div class="rt-container">
+        <div class="col-rt-12">
+          <div class="Scriptcontent">
+            <!-- Student Profile -->
+            <div class="student-profile py-4">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-4">
+                    <div class="card shadow-sm">
+                      <div class="card-header">
+                        <img
+                          class="profile_img"
+                          src="https://source.unsplash.com/600x300/?student"
+                          alt="student dp"
+                        />
+                        <h3>Aanandhene</h3>
+                      </div>
+                      <hr />
+                      <div class="card-body">
+                        <p class="mb-0">
+                          <strong class="pr-1">RollNumber:</strong>20CSR051
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-8">
+                    <div class="card shadow-sm2">
+                      <div class="card-header border-0">
+                        <h3 class="mb-0">
+                          <i class="far fa-clone pr-1"></i>General Information
+                        </h3>
+                      </div>
+                      <div class="card-body pt-0">
+                        <table class="table table-bordered" border="0">
+                          <tr>
+                            <th width="20%">class</th>
+                            <td width="2%">:</td>
+                            <td>A</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">year</th>
+                            <td width="2%">:</td>
+                            <td>II</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">department</th>
+                            <td width="2%">:</td>
+                            <td>CSE</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">DOB</th>
+                            <td width="2%">:</td>
+                            <td>12.09.2001</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">bloodGroup</th>
+                            <td width="2%">:</td>
+                            <td>B+</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">roomNumber</th>
+                            <td width="2%">:</td>
+                            <td>205</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">hostelName</th>
+                            <td width="2%">:</td>
+                            <td>Amaravathi</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">gender</th>
+                            <td width="2%">:</td>
+                            <td>Female</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">phoneNumber</th>
+                            <td width="2%">:</td>
+                            <td>9876506543</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">whatsappNumber</th>
+                            <td width="2%">:</td>
+                            <td>9873256754</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">telegramNumber</th>
+                            <td width="2%">:</td>
+                            <td>9876506543</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">collegeMailID</th>
+                            <td width="2%">:</td>
+                            <td>bhuva.20cse@kongu.edu</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">personalMailID</th>
+                            <td width="2%">:</td>
+                            <td>bhuva13@gmail.com</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">aadharNumber</th>
+                            <td width="2%">:</td>
+                            <td>424618229565</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">fatherName</th>
+                            <td width="2%">:</td>
+                            <td>Raghu moorthy</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">fatherMobileNumber</th>
+                            <td width="2%">:</td>
+                            <td>8765432898</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">fatherOccupation</th>
+                            <td width="2%">:</td>
+                            <td>office staff</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">motherName</th>
+                            <td width="2%">:</td>
+                            <td>nagarathna</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">motherMobileNumber</th>
+                            <td width="2%">:</td>
+                            <td>7654387652</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">MotherOccupation</th>
+                            <td width="2%">:</td>
+                            <td>home maker</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">address</th>
+                            <td width="2%">:</td>
+                            <td>
+                              no-1/45,kalahasthipuram(vill),avalapalli(pst),hosur(tk).
+                            </td>
+                          </tr>
+                          <tr>
+                            <th width="20%">district</th>
+                            <td width="2%">:</td>
+                            <td>krishnagiri</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">state</th>
+                            <td width="2%">:</td>
+                            <td>tamilnadu</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">localGuardian</th>
+                            <td width="2%">:</td>
+                            <td>madhavan</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">localGuardianPhoneNumber</th>
+                            <td width="2%">:</td>
+                            <td>8765438765</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">accountNumber</th>
+                            <td width="2%">:</td>
+                            <td>7654321234567890</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">bankName</th>
+                            <td width="2%">:</td>
+                            <td>karur vysya bank</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">branch</th>
+                            <td width="2%">:</td>
+                            <td>hosur</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">IFSCcode</th>
+                            <td width="2%">:</td>
+                            <td>8765432189</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">dateOfEntry</th>
+                            <td width="2%">:</td>
+                            <td>01.09.2021</td>
+                          </tr>
+                          <tr>
+                            <th width="20%">dateOfVacation</th>
+                            <td width="2%">:</td>
+                            <td>-</td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                    <!-- <div style="height: 26px"></div> -->
+                    <!-- <div class="card shadow-sm">
+                      <div class="card-header border-0">
+                        <h3 class="mb-0">
+                          <i class="far fa-clone pr-1"></i>Other Information
+                        </h3>
+                      </div>
+                      <div class="card-body pt-0">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat.
+                        </p>
+                      </div>
+                    </div> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- partial -->
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Analytics -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "staffInfo",
+  data() {
+    return {
+      user: this.$store.state.user,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.rt-heading {
+  margin-top: 80px;
+  text-align: center;
+  font-family: system-ui;
+}
+
+.profile_img {
+  border-radius: 100%;
+  border: 15px solid rgb(197, 193, 193);
+  width: 200px;
+  height: 200px;
+}
+
+.card {
+  width: 400px;
+  height: auto;
+  position: relative;
+  border-radius: 20px;
+  background-color: aliceblue;
+  box-shadow: 2px 10px 10px gray;
+  text-align: center;
+  margin-left: 70px;
+  font-family: system-ui;
+  font-weight: bold;
+  padding: 15px;
+}
+
+.table {
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+  border: 2px solid rgb(194, 186, 186);
+}
+
+.shadow-sm2 {
+  width: 80%;
+  align-items: center;
+  text-align: left;
+}
+
+table {
+  border-collapse: collapse;
+}
+
+td,
+th + th {
+  border-left: 1px solid rgb(107, 109, 106);
+}
+
+tr + tr {
+  border-top: 1px solid rgb(126, 124, 128);
+}
+
+td,
+th {
+  padding: 10px;
+}
+
+tr + tr > td,
+tr + tr > th {
+  border-top: 1px solid rgb(103, 102, 104);
+}
+
+.row {
+  display: grid;
+  grid-template-columns: 0.1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  justify-content: center;
+  padding: 10px;
+}
+
+@media (max-width: 790px) {
+  .row {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .profile_img {
+    width: 100px;
+    height: 200px;
+  }
+  .card {
+    width: 350px;
+    height: auto;
+    position: relative;
+    margin-left: 20px;
+    font-family: system-ui;
+    font-weight: bold;
+    padding: 15px;
+  }
+}
+</style>
--- kec/src/router/index.js
+++ kec/src/router/index.js
@@ -8,60 +8,65 @@ import Dashboard from "../views/dashBoard.vue";
 import staffDashboard from "../views/staffDashBoard.vue";
 import changePassword from "../components/resetPassword.vue";
 import permissions from "../components/permissions.vue";
-import studentRegistration from "../views/studentRegistration";
+import studentRegistration from "../views/studentRegistration.vue";
+import staffInfo from "../components/staffInfo.vue";
 // import paymentPage from "../components/dashboard/paymentPage";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginPop,
-  },
-  {
-    path: "/Student",
-    name: "studentInfo",
-    component: StudentInfo,
-  },
-  {
-    path: "/booking",
-    name: "BookingView",
-    component: BookingView,
-  },
-  {
-    path: "/dash",
-    name: "homeview",
-    component: Dashboard,
-  },
-  {
-    path: "/staffDashboard",
-    name: "staffDashboard",
-    component: staffDashboard,
-  },
-  {
-    path: "/changePassword",
-    name: "changePassword",
-    component: changePassword,
-  },
-  {
-    path: "/permissions",
-    name: "permissionPage",
-    component: permissions,
-  },
-  {
-    path: "/registration",
-    name: "studentRegisration",
-    component: studentRegistration,
-  },
+const routes = [{
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: LoginPop,
+    },
+    {
+        path: "/Student",
+        name: "studentInfo",
+        component: StudentInfo,
+    },
+    {
+        path: "/booking",
+        name: "BookingView",
+        component: BookingView,
+    },
+    {
+        path: "/dash",
+        name: "homeview",
+        component: Dashboard,
+    },
+    {
+        path: "/staffDashboard",
+        name: "staffDashboard",
+        component: staffDashboard,
+    },
+    {
+        path: "/changePassword",
+        name: "changePassword",
+        component: changePassword,
+    },
+    {
+        path: "/permissions",
+        name: "permissionPage",
+        component: permissions,
+    },
+    {
+        path: "/registration",
+        name: "studentRegisration",
+        component: studentRegistration,
+    },
+    {
+        path: "/staffInfo",
+        name: "staffInfo",
+        component: staffInfo,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
