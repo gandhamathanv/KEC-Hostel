cfff68ee0cbbaa3f0a96aecde26b31c0e987444d
AANANDHENE M <91001392+Aanandhene@users.noreply.github.com>
2022-05-20 14:41:30 +0530

    headernav changed

--- kec/src/components/HeaderNav.vue
+++ kec/src/components/HeaderNav.vue
@@ -1,157 +1,125 @@
 <template>
   <div>
-    <nav class="header-nav">
-      <span @click="navigateTo({ name: 'home' })"
-        ><img src="@/assets/img/homepage/keclogo.png"
-      /></span>
-      <div class="nav-links" id="nav-links">
-        <!-- <i class="fa fa-times" Onclick="hideMenu()"></i> -->
+    <nav class="nav">
+      <div class="logo">KONGU HOSTEL</div>
+      <input type="checkbox" id="click" />
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul class="ul">
+        <li class="li"><a href="#">About</a></li>
 
-        <ul>
-          <li>
-            <span href="#">HOME</span>
-          </li>
-          <li>
-            <span @click="navigateTo({ name: 'login' })">LOGIN</span>
-          </li>
-        </ul>
-      </div>
-      <i class="fa fa-bars"></i
-      ><!---onclick="showMenu()"--->
+        <li class="li"><a class="active" href="#">Login</a></li>
+      </ul>
     </nav>
   </div>
 </template>
 
-<script type="text/javascript">
-export default {
-  methods: {
-    togglemenu() {
-      var menutoggle = document.querySelector(".toggle");
-      var menu = document.querySelector(".menu");
-      menutoggle.classList.toggle("active");
-      menu.classList.toggle("active");
-    },
-    showLogin() {
-      this.loginPop = !this.loginPop;
-    },
-    logout() {
-      this.$store.dispatch("setToken", null);
-      this.$store.dispatch("setUser", null);
-      this.$router.push({
-        name: "home",
-      });
-    },
-    navigateTo(route) {
-      this.$router.push(route);
-    },
-  },
-  data() {
-    return {
-      loginPop: false,
-    };
-  },
-};
-// $(document).ready(function () {
-//   var showHeaderAt = 150;
+<script type="text/javascript"></script>
 
-//   var win = $(window),
-//     body = $("body");
-
-//   if (win.width() > 400) {
-//     win.on("scroll", function (e) {
-//       if (win.scrollTop() > showHeaderAt) {
-//         body.addClass("fixed");
-//       } else {
-//         body.removeClass("fixed");
-//       }
-//     });
-//   }
-// });
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-nav {
+* {
+  margin: 0px;
+
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+.nav {
   display: flex;
-  padding: 0% 6%;
-  /* padding-bottom: 30pc; */
-  justify-content: space-between;
-  align-items: center;
-}
-nav img {
-  width: 75px;
-}
-.nav-links {
-  flex: 1;
-  text-align: right;
-  color: black;
-}
-.nav-links ul li {
-  list-style: none;
-  display: inline-block;
-  padding: 8px 12px;
-  position: relative;
-}
-.nav-links ul li::after {
-  content: "";
-  width: 0%;
-  height: 2px;
-  cursor: pointer;
-  background: #f44336;
-  display: block;
-  margin: auto;
-  transition: 0.5s;
-}
-.nav-links ul li:hover::after {
+  height: 80px;
   width: 100%;
+  background: #1b1b1b;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px 0 100px;
+  flex-wrap: wrap;
 }
-.text-box {
-  width: 90%;
+.nav .logo {
   color: #fff;
-  position: absolute;
-  top: 42%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
+  font-size: 36px;
+  font-family: serif;
+
+  color: rgb(229, 94, 63);
+
+  font-weight: 600;
 }
-.text-box h1 {
-  font-size: 62px;
+.nav .ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
 }
-.text-box p {
-  margin: 10px 0 40px;
-  font-size: 14px;
+.nav .ul .li {
+  margin: 0 5px;
+}
+.nav .ul .li a {
+  color: #f2f2f2;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 8px 15px;
+  border-radius: 5px;
+  letter-spacing: 1px;
+  transition: all 0.3s ease;
+}
+.nav .ul .li a.active,
+.nav .ul .li a:hover {
+  color: #111;
+  background: #fff;
+}
+.nav .menu-btn i {
   color: #fff;
-}
-nav .fa {
+  font-size: 22px;
+  cursor: pointer;
   display: none;
 }
-@media (max: width 700px) {
-  .text-box h1 {
-    font-size: 20px;
+input[type="checkbox"] {
+  display: none;
+}
+@media (max-width: 1000px) {
+  .nav {
+    padding: 0 40px 0 50px;
   }
-  .nav-links ul li {
+}
+@media (max-width: 920px) {
+  .nav .menu-btn i {
     display: block;
   }
-  .nav-links {
-    position: absolute;
-    background: #f44336;
+  #click:checked ~ .menu-btn i:before {
+    content: "\f00d";
+  }
+  .nav .ul {
+    position: fixed;
+    top: 80px;
+    left: -100%;
+    background: #111;
     height: 100vh;
-    width: 200px;
-    top: 0;
-    right: -200px;
-    text-align: left;
-    z-index: 2;
-    transition: 1s;
-  }
-  nav .fa {
+    width: 100%;
+    text-align: center;
     display: block;
-    color: #fff;
-    margin: 10px;
-    font-size: 22px;
-    cursor: pointer;
+    transition: all 0.3s ease;
   }
-  .nav-links ul {
-    padding: 30px;
+  #click:checked ~ .ul {
+    left: 0;
+  }
+  .nav .ul .li {
+    width: 100%;
+    margin: 40px 0;
+  }
+  .nav .ul .li a {
+    width: 100%;
+    margin-left: -100%;
+    display: block;
+    font-size: 20px;
+    transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  }
+  #click:checked ~ .ul .li a {
+    margin-left: 0px;
+  }
+  .nav .ul .li a.active,
+  .nav .ul .li a:hover {
+    background: none;
+    color: cyan;
   }
 }
 </style>
