c9b187faa0a06c7f9c36e3b4f6e65a47f4de0c49
AANANDHENE M <91001392+Aanandhene@users.noreply.github.com>
2022-05-29 11:10:25 +0530

    loading page created

--- kec/src/components/updateInfo.vue
+++ kec/src/components/updateInfo.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <!-- Analytics -->
-    updateInfo
+    <div class="loading">
+      <span>Loading</span>
+    </div>
   </div>
 </template>
 
@@ -14,5 +15,69 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "montserrat", sans-serif;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+}
+
+.loading {
+  margin: auto;
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  border-radius: 50%;
+  border-top: 10px solid #706fd3;
+  position: relative;
+  top: 50px;
+  animation: a1 2s linear infinite;
+}
+
+.loading::before,
+.loading::after {
+  content: "";
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  left: 0;
+  top: -10px;
+  box-sizing: border-box;
+  border-radius: 50%;
+}
+
+.loading::before {
+  border-top: 10px solid #40407a;
+  transform: rotate(120deg);
+}
+.loading::after {
+  border-top: 10px solid #474787;
+  transform: rotate(240deg);
+}
+
+.loading span {
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  color: rgb(23, 21, 21);
+  text-align: center;
+  line-height: 200px;
+  animation: a2 2s linear infinite;
+  z-index: 4;
+}
+
+@keyframes a1 {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes a2 {
+  to {
+    transform: rotate(-360deg);
+  }
+}
+</style>
