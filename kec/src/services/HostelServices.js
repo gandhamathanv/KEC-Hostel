067835984c6233b8e5f46a76712c95653d3cc2ce
gandhamathan <gandhamathanv.20cse@kongu.edu>
2022-06-05 14:05:38 +0530

    commit

--- kec/src/services/HostelServices.js
+++ kec/src/services/HostelServices.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("jwt");
 export default {
   getHostels(credentials) {
     console.log(credentials);
-    return Api().post("getHostels", Credentials, {
+    return Api().post("getHostels", credentials, {
       headers: {
         Authorization: "Bearer " + token,
       },
@@ -12,7 +12,7 @@ export default {
   },
   getRooms(credentials) {
     console.log(credentials);
-    return Api().post("getRooms", Credentials, {
+    return Api().post("getRooms", credentials, {
       headers: {
         Authorization: "Bearer " + token,
       },
@@ -34,21 +34,21 @@ export default {
     });
   },
   getPermissions(credentials) {
-    return Api().post("getPermissions", Credentials, {
+    return Api().post("getPermissions", credentials, {
       headers: {
         Authorization: "Bearer " + token,
       },
     });
   },
   setPermissions(credentials) {
-    return Api().patch("setPermissions", Credentials, {
+    return Api().patch("setPermissions", credentials, {
       headers: {
         Authorization: "Bearer " + token,
       },
     });
   },
   closePermissions(credentials) {
-    return Api().patch("closePermissions", Credentials, {
+    return Api().patch("closePermissions", credentials, {
       headers: {
         Authorization: "Bearer " + token,
       },
