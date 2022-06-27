550f49fa8e1f4de9c27a3bdbec355457c50e02b1
gandhamathan <gandhasaroja@gmail.com>
2022-06-27 21:07:21 +0530

    final etmp commit checking

--- kec/src/router/index.js
+++ kec/src/router/index.js
@@ -42,223 +42,220 @@ import exportData from "../components_Dash/container/ExportContainer.vue";
 import userInfo from "../components_Dash/container/UserInfo.vue";
 import DashboardBookNow from "../views/BookingView.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
+const routes = [{
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
 
-  {
-    path: "/login",
-    name: "login",
-    component: LoginPop,
-  },
-  {
-    path: "/Student",
-    name: "studentInfo",
-    component: StudentInfo,
-  },
-  {
-    path: "/booking",
-    name: "BookingView",
-    component: BookingView,
-  },
-  {
-    path: "/studentDashboard",
-    name: "studentDashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/staffDashboard",
-    name: "staffDashboard",
-    component: staffDashboard,
-  },
-  {
-    path: "/changePassword",
-    name: "changePassword",
-    component: changePassword,
-  },
-  {
-    path: "/permissions",
-    name: "permissionsPage",
-    component: permissions,
-  },
-  {
-    path: "/studentRegistration",
-    name: "studentRegisration",
-    component: studentRegistration,
-  },
-  {
-    path: "/staffRegistration",
-    name: "staffRegisration",
-    component: staffRegistration,
-  },
-  {
-    path: "/staffInfo",
-    name: "staffInfo",
-    component: staffInfo,
-  },
-
-  {
-    path: "/updateInfo",
-    name: "updateInfo",
-    component: updateInfo,
-  },
-
-  {
-    path: "/popup",
-    name: "popup",
-    component: Popup,
-  },
-  {
-    path: "/studentDataList",
-    name: "studentDataList",
-    component: studentDataList,
-  },
-  {
-    path: "/studentDataList/:rollnumber",
-    name: "studentdetail",
-    component: studentDetail,
-  },
-
-  {
-    path: "/staffDataList",
-    name: "staffDataList",
-    component: staffDataList,
-  },
-  {
-    path: "/staffDataList/:staffID",
-    name: "staffDetail",
-    component: staffDetail,
-  },
-  {
-    path: "/bookingDataList",
-    name: "bookingDataList",
-    component: bookingDataList,
-  },
-  {
-    path: "/roomDataList/:hostelName",
-    name: "roomDataList",
-    component: roomDataList,
-  },
-  {
-    path: "/hostelDataList",
-    name: "hostelDataList",
-    component: hostelDataList,
-  },
-  {
-    path: "/notificationList",
-    name: "notificationList",
-    component: notificationList,
-  },
-  {
-    path: "/hostelDataList/:hostelName",
-    name: "hostelDetail",
-    component: hostelDetail,
-  },
-  {
-    path: "/roomDataList/:hostelName/:roomNumber",
-    name: "roomDetail",
-    component: roomDetail,
-  },
-  {
-    path: "/bookingDataList/:rollnumber",
-    name: "bookingDetail",
-    component: bookingDetail,
-  },
-  {
-    path: "/exportData",
-    name: "exportData",
-    component: exportData,
-  },
-  {
-    path: "/loadingPage",
-    name: "loadingPage",
-    component: loadingPage,
-  },
-  {
-    path: "/loaderPage",
-    name: "loaderPage",
-    component: loaderPage,
-  },
-  {
-    path: "/newdash",
-    name: "NewDash",
-    component: newDash,
-  },
-
-  //new routes
-  {
-    path: "/dashboard/home",
-    component: Dashboard,
-    name: "DashboardHome",
-    children: [
-      {
-        path: "",
-        component: overView,
-        name: "DashboardOverview",
-      },
-
-      {
-        path: "staffList",
-        component: DashboardStaffList,
-        name: "DashboardStaff",
-      },
-      {
-        path: "studentList",
-        component: DashboardStudentList,
-        name: "DashboardStudent",
-      },
-      {
-        path: "BookingList",
-        component: DashboardBookingList,
-        name: "DashboardBooking",
-      },
-      {
-        path: "userinfo",
-        name: "UserInfo",
-        component: userInfo,
-      },
-    ],
-  },
-  {
-    path: "/dashboard",
-    component: Dashboard,
-    name: "Dashboard",
-    children: [
-      {
-        path: "changepassword",
+    {
+        path: "/login",
+        name: "login",
+        component: LoginPop,
+    },
+    {
+        path: "/Student",
+        name: "studentInfo",
+        component: StudentInfo,
+    },
+    {
+        path: "/booking",
+        name: "BookingView",
+        component: BookingView,
+    },
+    {
+        path: "/studentDashboard",
+        name: "studentDashboard",
+        component: Dashboard,
+    },
+    {
+        path: "/staffDashboard",
+        name: "staffDashboard",
+        component: staffDashboard,
+    },
+    {
+        path: "/changePassword",
+        name: "changePassword",
         component: changePassword,
-        name: "DashboardChangePassword",
-      },
-      {
-        path: "permissions",
+    },
+    {
+        path: "/permissions",
+        name: "permissionsPage",
         component: permissions,
-        name: "DashboardPermission",
-      },
-      {
-        path: "Menu",
-        name: "todaysMenu",
-        component: todaysMenu,
-      },
-      {
-        path: "export",
+    },
+    {
+        path: "/studentRegistration",
+        name: "studentRegisration",
+        component: studentRegistration,
+    },
+    {
+        path: "/staffRegistration",
+        name: "staffRegisration",
+        component: staffRegistration,
+    },
+    {
+        path: "/staffInfo",
+        name: "staffInfo",
+        component: staffInfo,
+    },
+
+    {
+        path: "/updateInfo",
+        name: "updateInfo",
+        component: updateInfo,
+    },
+
+    {
+        path: "/popup",
+        name: "popup",
+        component: Popup,
+    },
+    {
+        path: "/studentDataList",
+        name: "studentDataList",
+        component: studentDataList,
+    },
+    {
+        path: "/studentDataList/:rollnumber",
+        name: "studentdetail",
+        component: studentDetail,
+    },
+
+    {
+        path: "/staffDataList",
+        name: "staffDataList",
+        component: staffDataList,
+    },
+    {
+        path: "/staffDataList/:staffID",
+        name: "staffDetail",
+        component: staffDetail,
+    },
+    {
+        path: "/bookingDataList",
+        name: "bookingDataList",
+        component: bookingDataList,
+    },
+    {
+        path: "/roomDataList/:hostelName",
+        name: "roomDataList",
+        component: roomDataList,
+    },
+    {
+        path: "/hostelDataList",
+        name: "hostelDataList",
+        component: hostelDataList,
+    },
+    {
+        path: "/notificationList",
+        name: "notificationList",
+        component: notificationList,
+    },
+    {
+        path: "/hostelDataList/:hostelName",
+        name: "hostelDetail",
+        component: hostelDetail,
+    },
+    {
+        path: "/roomDataList/:hostelName/:roomNumber",
+        name: "roomDetail",
+        component: roomDetail,
+    },
+    {
+        path: "/bookingDataList/:rollnumber",
+        name: "bookingDetail",
+        component: bookingDetail,
+    },
+    {
+        path: "/exportData",
         name: "exportData",
         component: exportData,
-      },
-    ],
-  },
-  {
-    path: "/:catchAll(.*)",
-    name: "errorStatus",
-    component: errorStatus,
-  },
+    },
+    {
+        path: "/loadingPage",
+        name: "loadingPage",
+        component: loadingPage,
+    },
+    {
+        path: "/loaderPage",
+        name: "loaderPage",
+        component: loaderPage,
+    },
+    {
+        path: "/newdash",
+        name: "NewDash",
+        component: newDash,
+    },
+
+    //new routes
+    {
+        path: "/dashboard/home",
+        component: Dashboard,
+        name: "DashboardHome",
+        children: [{
+                path: "",
+                component: overView,
+                name: "DashboardOverview",
+            },
+
+            {
+                path: "staffList",
+                component: DashboardStaffList,
+                name: "DashboardStaff",
+            },
+            {
+                path: "studentList",
+                component: DashboardStudentList,
+                name: "DashboardStudent",
+            },
+            {
+                path: "BookingList",
+                component: DashboardBookingList,
+                name: "DashboardBooking",
+            },
+            {
+                path: "DashboardBookNow",
+                name: "DashboardBookNow",
+                component: DashboardBookNow,
+            },
+        ],
+    },
+    {
+        path: "/dashboard",
+        component: Dashboard,
+        name: "Dashboard",
+        children: [{
+                path: "changepassword",
+                component: changePassword,
+                name: "DashboardChangePassword",
+            },
+            {
+                path: "permissions",
+                component: permissions,
+                name: "DashboardPermission",
+            },
+            {
+                path: "Menu",
+                name: "todaysMenu",
+                component: todaysMenu,
+            },
+            {
+                path: "export",
+                name: "exportData",
+                component: exportData,
+            },
+        ],
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "errorStatus",
+        component: errorStatus,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
